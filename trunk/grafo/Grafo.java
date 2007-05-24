aed5e214571d0be74b69e19883560264d4ef448d
matiasblanco18 <matiasblanco18@5fb46292-2c31-0410-98c9-d3d389809fd0>
2007-05-24 03:40:23 +0000

    Grafo alpha version

--- trunk/grafo/Grafo.java
+++ trunk/grafo/Grafo.java
@@ -1,21 +1,42 @@
 package grafo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Grafo {
 
 	public int nodos;
 	public List<Eje> ejes;
-	public List<List<Eje>> adyacencias;
+	public List<List<Integer>> adyacencias;
 	
 	public Grafo(int nodos,List<Eje> ejes){
 		
 		this.nodos = nodos;
 		this.ejes = ejes;
 		
+		List<List<Integer>> ady = new ArrayList<List<Integer>>();
 		
+		int i = 1;
+		List<Integer> temp;
 		
+		while(i <= nodos){		
+			int k = 0;
+			temp = new ArrayList<Integer>();
+			
+			while(k < ejes.size()){			
+				if(ejes.get(k).x == i){
+					temp.add(ejes.get(k).y);
+				}			
+				if(ejes.get(k).y == i){
+					temp.add(ejes.get(k).x);
+				}
+				k++;
+			}
+			ady.add(i, temp);
+			i++;
+		}
 		
+		this.adyacencias = ady;
 	}
 	
 	
@@ -27,8 +48,8 @@ public class Grafo {
 		return this.ejes;
 	}
 	
-	public List<Eje> DameVecinos(int nodo){
-		return null;
+	public List<Integer> DameVecinos(int nodo){
+		return this.adyacencias.get(nodo);
 	}
 	
 }
