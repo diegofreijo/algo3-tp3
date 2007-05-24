83f196969f45f3c860cff40f06cdc4cbe6389386
matiasblanco18 <matiasblanco18@5fb46292-2c31-0410-98c9-d3d389809fd0>
2007-05-24 04:06:05 +0000

    Grafo beta
    Mañana hago el script para las instancias, o te lo mando desde el sur.

--- grafo/Grafo.java
+++ grafo/Grafo.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 public class Grafo {
 
-	public int nodos;
-	public List<Eje> ejes;
-	public List<List<Integer>> adyacencias;
+	private int nodos;
+	private List<Eje> ejes;
+	private List<List<Integer>> adyacencias;
 	
 	public Grafo(int nodos,List<Eje> ejes){
 		
@@ -15,14 +15,15 @@ public class Grafo {
 		this.ejes = ejes;
 		
 		List<List<Integer>> ady = new ArrayList<List<Integer>>();
+		ady.add(0, null);
 		
+		List<Integer> temp = new ArrayList<Integer>();
 		int i = 1;
-		List<Integer> temp;
 		
 		while(i <= nodos){		
 			int k = 0;
 			temp = new ArrayList<Integer>();
-			
+		
 			while(k < ejes.size()){			
 				if(ejes.get(k).x == i){
 					temp.add(ejes.get(k).y);
