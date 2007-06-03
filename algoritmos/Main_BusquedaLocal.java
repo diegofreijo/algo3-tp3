7314f328e5589d69cd69995faa885ac324c6bb1e
giga.freijo <giga.freijo@5fb46292-2c31-0410-98c9-d3d389809fd0>
2007-06-03 18:51:55 +0000

    Agregue la clase Recubrimiento como una utilidad. Hice la clase Estadisticas. Modifique el parser para que lea todos los grafos de una familia y para que escriba las estadisticas y los tamaños de los recubrimientos del algoritmo dado. Modifique el Main para que use todo lo que acabo de hacer.
    Cree carpeta bib para bibliografias.

--- algoritmos/Main_BusquedaLocal.java
+++ algoritmos/Main_BusquedaLocal.java
@@ -1,20 +1,15 @@
 package algoritmos;
 
-import java.util.List;
-
-import tp3.Parser;
-import utilidades.Grafo;
-
 public class Main_BusquedaLocal
 {
 	public static void main(String[] args)
 	{
-		Grafo g = Parser.LeerGrafo("in/ej.in");
+		/*Grafo g = Parser.LeerGrafo("in/ej.in");
 		
 		System.out.println("Leido:\n" + g);
 		
 		List<Integer> recubrimiento = BusquedaLocal.Ejecutar(g);
 		
-		System.out.println("Busqueda local: " + recubrimiento.toString());		
+		System.out.println("Busqueda local: " + recubrimiento.toString());*/	
 	}
 }
