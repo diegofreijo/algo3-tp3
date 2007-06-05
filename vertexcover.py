33d38ab96e3c4d2622ea8af74f9733f96840bafc
matiasblanco18 <matiasblanco18@5fb46292-2c31-0410-98c9-d3d389809fd0>
2007-06-05 02:45:40 +0000

    Version beta que tendria que funcionar para exacto, goloso y busqueda

--- vertexcover.py
+++ vertexcover.py
@@ -28,20 +28,47 @@ if(error == "false"):
     if algo == 1:
         print "Corriendo Algoritmo Exacto"
         tiempoInicial = time.time()
-        proc = subprocess.Popen("java -jar goloso.jar", shell=True)
+        proc = subprocess.Popen("java -jar exacto.jar " + caso, shell=True)
         proc.wait()
         tiempoFinal = time.time()
         print "El algoritmo tardo(en ms):" + str(tiempoFinal - tiempoInicial)
-        dat = open("dat/Tp3(busqueda_local).dat","w")
-        time.sleep(2)
+        dat = open("dat/Tp3(exacto).dat","r")
+        instrucciones = dat.readline()
+        instrucciones = instrucciones.partition(" ")
+        instrucciones = instrucciones[2]
+        print "La cantidad de instrucciones basicas fueron:" + instrucciones
     if algo == 2:
         print "Corriendo Goloso"
-        tiempo = time()
-        #Aca va la parte del subprocess del algoritmo
+        tiempoInicial = time.time()
+        proc = subprocess.Popen("java -jar goloso.jar " + caso, shell=True)
+        proc.wait()
+        tiempoFinal = time.time()
+        print "El algoritmo tardo(en ms):" + str(tiempoFinal - tiempoInicial)
+        dat = open("dat/Tp3(goloso).dat","r")
+        instrucciones = dat.readline()
+        instrucciones = instrucciones.partition(" ")
+        instrucciones = instrucciones[2]
+        print "La cantidad de instrucciones basicas fueron:" + instrucciones
     if algo == 3:
-        print "Corriendo Busqueda Local"
-        tiempo = time()
-        #Aca va la parte del subprocess del algoritmo
+        listo = "0"
+        while listo = "0":
+            meto = raw_input("Cuantos nodos quiere agregar?" )
+            saco = raw_input("Cuantos nodos quiere sacar?" )
+            if saco <= meto:
+                listo = "0"
+                print "La cantidad que saca tiene que ser mayor a la que ingresa"
+            else:
+                listo = "1"
+        tiempoInicial = time.time()
+        proc = subprocess.Popen("java -jar busqueda_local.jar " + caso + " " + saco + " " + meto, shell=True)
+        proc.wait()
+        tiempoFinal = time.time()
+        print "El algoritmo tardo(en ms):" + str(tiempoFinal - tiempoInicial)
+        dat = open("dat/Tp3(busqueda_local).dat","r")
+        instrucciones = dat.readline()
+        instrucciones = instrucciones.partition(" ")
+        instrucciones = instrucciones[2]
+        print "La cantidad de instrucciones basicas fueron:" + instrucciones
     if algo == 4:
         print "Corriendo GRASP"
         tiempo = time()
