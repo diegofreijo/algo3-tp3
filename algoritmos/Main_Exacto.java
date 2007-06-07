630c99283c5ec8d550bfea6fca4fc025d0d82d4f
matiasblanco18 <matiasblanco18@5fb46292-2c31-0410-98c9-d3d389809fd0>
2007-06-07 01:26:20 +0000

    Exacto version beta, con un par de pruebas encima, pero parece que anda.
    Despues lo sigo probando

---
+++ algoritmos/Main_Exacto.java
@@ -0,0 +1,25 @@
+package algoritmos;
+
+import java.util.List;
+
+import utilidades.Grafo;
+import utilidades.Parser;
+
+public class Main_Exacto {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Grafo g = Parser.LeerGrafo("in/ej.in");
+		
+		System.out.println("Leido:\n" + g);
+		
+		List<Integer> recubrimiento = Exacto.Ejecutar(g);
+		
+		System.out.println("Exacto: " + recubrimiento.toString());		
+
+	}
+
+}
