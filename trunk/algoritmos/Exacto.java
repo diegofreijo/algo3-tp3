1973d02b49d49049896236e83d250e9dbfcf4408
matiasblanco18 <matiasblanco18@5fb46292-2c31-0410-98c9-d3d389809fd0>
2007-06-07 00:32:35 +0000

    Algoritmo Exacto Alpha

---
+++ trunk/algoritmos/Exacto.java
@@ -0,0 +1,81 @@
+package algoritmos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import utilidades.Eje;
+import utilidades.Grafo;
+
+import utilidades.Recubrimiento;
+
+public class Exacto {
+
+	public static List<Integer> Ejecutar(Grafo g){
+		
+		List<Integer> ret = new ArrayList<Integer>();
+		
+		List<Integer> nodos = SacarAislados(g.DameNodos(),g.DameAislados());
+		
+		List<Integer> sol = new ArrayList<Integer>();
+		
+		ret =  AlgoExacto(nodos,g,sol,nodos);
+		
+		return null;
+	}
+
+	private static List<Integer> AlgoExacto(List<Integer> nodos, Grafo g, List<Integer> sol, List<Integer> min) {
+		
+		if(EsRecubrimiento(sol,g)){
+			if(sol.size() < min.size()){
+				min = sol;
+			}
+			return min;
+		} else {
+			if(!nodos.isEmpty()){
+				int nodo = nodos.get(0);
+				nodos.remove(nodo);
+				List<Integer> temp = sol;
+				temp.add(nodo);
+				List<Integer> sin = AlgoExacto(nodos,g,sol,min);
+				List<Integer> con = AlgoExacto(nodos,g,temp,min);
+				if (sin.size() < con.size()){
+					return sin;
+				} else {
+					return con;
+				}
+			} else {
+				
+				return min;
+			}
+		}
+	}
+	
+	public static List<Integer> SacarAislados(int n, List<Integer> aislados) {
+		
+		List<Integer> ret = new ArrayList<Integer>();
+		int i = 1;
+		
+		while(i <= n){
+			if(!aislados.contains(i)){
+				ret.add(i);
+			}
+			i++;
+		}
+		
+		return ret;
+	}
+	
+	private static boolean EsRecubrimiento(List<Integer> solucion, Grafo g)
+    {
+		for(Eje e : g.DameEjes())
+		{
+			if(!solucion.contains(Integer.valueOf(e.x)) && !solucion.contains(Integer.valueOf(e.y)))
+			{
+				return false;
+			}
+		}
+		
+	    return true;
+    }
+
+}
