fc9f683ec4209c4d1d5a547c92ed81359b760833
matiasblanco18 <matiasblanco18@5fb46292-2c31-0410-98c9-d3d389809fd0>
2007-06-10 00:03:42 +0000

    Clase GolosoRandom.
    Igualita a Goloso, pero genera una lista con los que tienen la misma cantidad de vecinos, y agarra uno al azar a partir de una semilla.

--- algoritmos/GolosoRandom.java
+++ algoritmos/GolosoRandom.java
@@ -2,13 +2,14 @@ package algoritmos;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import utilidades.Estadisticas;
 import utilidades.Grafo;
 import utilidades.Recubrimiento;
 
-public abstract class Goloso {
-	
+public class GolosoRandom {
+
 	private static Estadisticas e;
 	
 	public static Recubrimiento Ejecutar(Grafo g, Estadisticas est){
@@ -22,9 +23,12 @@ public abstract class Goloso {
 		System.out.println("Ejes: " + g.DameEjes());		
 		
 		while(solucion.EsRecubrimiento(g)){
+			++e.i;
 			Integer nodoMayor = NodoMayorGrado(nodos,g);
+			++e.i;
 			solucion.nodos.add(nodoMayor);
-			System.out.println("Ejes Cubiertos: " + g.DameVecinos(nodoMayor).toString());		
+			System.out.println("Ejes Cubiertos: " + g.DameVecinos(nodoMayor).toString());
+			++e.i;
 			nodos.remove(nodoMayor);
 		}
 		
@@ -36,30 +40,47 @@ public abstract class Goloso {
 		
 		int i = 0;
 		
-		int ret = nodos.get(1);
+		List<Integer> temp = new ArrayList<Integer>();
+		++e.i;
+		int mayor = nodos.get(1);
 		
 		while(i < nodos.size()){
-			if(g.DameVecinos(ret).size() < g.DameVecinos(nodos.get(i)).size()){
-				ret = nodos.get(i);
+			++e.i;
+			if(g.DameVecinos(mayor).size() < g.DameVecinos(nodos.get(i)).size()){
+				++e.i;
+				mayor = nodos.get(i);++e.i;
+				temp.clear();++e.i;
+				temp.add(nodos.get(i));++e.i;
+			} else if (g.DameVecinos(mayor).size() == g.DameVecinos(nodos.get(i)).size()) {
+				++e.i;
+				temp.add(nodos.get(i));++e.i;
 			}
+			++e.i;
 			i++;
 		}
 		
-		return ret;
+		Random rnd = new Random(temp.size() + 1235689 / 10 * 8);
+		
+		int ret = rnd.nextInt(temp.size());++e.i;
+		
+		return temp.get(ret);
 	}
 
 	public static List<Integer> SacarAislados(int n, List<Integer> aislados) {
 		
 		List<Integer> ret = new ArrayList<Integer>();
-		int i = 1;
+		int i = 1;++e.i;
 		
 		while(i <= n){
+			++e.i;
 			if(!aislados.contains(i)){
-				ret.add(i);
+				++e.i;
+				ret.add(i);++e.i;
 			}
-			i++;
+			i++;++e.i;
 		}
 		
 		return ret;
 	}
+	
 }
