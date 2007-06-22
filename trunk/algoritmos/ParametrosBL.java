c56d575ca7aa913dadf29c5ba93e3773baaf8bef
giga.freijo <giga.freijo@5fb46292-2c31-0410-98c9-d3d389809fd0>
2007-06-22 03:26:02 +0000

    No logro solucionar el puto bug del tamaño de puntajes.

--- trunk/algoritmos/ParametrosBL.java
+++ trunk/algoritmos/ParametrosBL.java
@@ -1,8 +1,15 @@
 package algoritmos;
 
+import java.util.Comparator;
+
 public class ParametrosBL
 {
 	public boolean mezclar_nodos_busqueda_vecino = false;
 	public int porcentaje_cuantos_saco;
-	public int porcentaje_cuantos_agrego; 
+	public int porcentaje_cuantos_agrego;
+	
+	public String toString()
+	{
+		return "(" + porcentaje_cuantos_agrego + "," + porcentaje_cuantos_saco + ")";
+	}
 }
