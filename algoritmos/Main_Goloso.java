aaa85384367bd1c34b2a2093a533a7c21e480c02
matiasblanco18 <matiasblanco18@5fb46292-2c31-0410-98c9-d3d389809fd0>
2007-06-25 13:46:22 +0000

    Python terminado, falta generar los .jar de bl y grasp

--- algoritmos/Main_Goloso.java
+++ algoritmos/Main_Goloso.java
@@ -11,20 +11,19 @@ public class Main_Goloso {
 
 public static void main(String[] args) {
 		
-		System.out.println("Ejecutando: Goloso");
 		Estadisticas est_goloso = new Estadisticas();
 		Grafico grafico = new Grafico("Goloso","goloso");
 		Recubrimiento recubrimiento;
-		//Grafo g = Parser.LeerGrafo(args[0]);
-		Grafo g = Parser.LeerGrafo("aleatorio1.in");
+		Grafo g = Parser.LeerGrafoPy(args[0]);
+		//Grafo g = Parser.LeerGrafo("aleatorio1.in");
 		
 		System.out.println("Grafo: " + g.toString());
 		recubrimiento = Goloso.Ejecutar(g,est_goloso);
 		Punto2D punto = new Punto2D(g.DameNodos(),est_goloso.i);
 		grafico.Agregar(punto);
-		Parser.EscribirGrafico(grafico);
-		//Parser.EscribirResultado(recubrimiento, args[0]);
-		Parser.EscribirResultado(recubrimiento, "aleatorio1.in");
+		Parser.EscribirGraficoPy(grafico,args[2]);
+		Parser.EscribirResultadoPy(recubrimiento, args[1]);
+		//Parser.EscribirResultado(recubrimiento, "aleatorio1.in");
 	}
 	
 }
--- vertexcover.py
+++ vertexcover.py
@@ -21,8 +21,14 @@ elif algo > 4:
     print "Debe ingresar un numero entre 1 y 4"
     error = true
 
-string = 'Ingrese el nombre del archivo a probar: '
+string = 'Ingrese la ruta del archivo a probar: '
 caso = raw_input(string)
+
+string = 'Ingrese la ruta del archivo de salida: '
+archout = raw_input(string)
+
+string = 'Ingrese la ruta del archivo de datos: '
+archdat = raw_input(string)
 
 nombreCaso = caso.partition(".")
 nombreCaso = caso[0]
@@ -31,35 +37,33 @@ if(error == "false"):
     if algo == 1:
         print "Corriendo Algoritmo Exacto"
         tiempoInicial = time.time()
-        proc = subprocess.Popen("java -jar exacto.jar " + caso, shell=True)
+        proc = subprocess.Popen("java -jar exacto.jar " + caso + " " +archout+ " " + archdat, shell=True)
         proc.wait()
         tiempoFinal = time.time()
         print "El algoritmo tardo(en ms):" + str(tiempoFinal - tiempoInicial)
-        dat = open("dat/Exacto/Exacto(exacto).dat","r")
+        dat = open(archdat,"r")
         instrucciones = dat.readline()
         instrucciones = instrucciones.partition(" ")
         instrucciones = instrucciones[2]
         print "La cantidad de instrucciones basicas fueron:" + instrucciones
-        out = open("out/"+nombreCaso+".out","r")
+        out = open(archout,"r")
         resultado = out.readline()
         print "La cantidad de nodos necesarios para realizar el recubrimiento fue:" + resultado
     if algo == 2:
         print "Corriendo Goloso"
         tiempoInicial = time.time()
-        proc = subprocess.Popen("java -jar goloso.jar " + caso, shell=True)
+        proc = subprocess.Popen("java -jar goloso.jar " + caso + " " +archout+ " " + archdat, shell=True)
         proc.wait()
         tiempoFinal = time.time()
         print "El algoritmo tardo(en ms):" + str(tiempoFinal - tiempoInicial)
-        dat = open("dat/Goloso/Goloso(goloso).dat","r")
+        dat = open(archdat,"r")
         instrucciones = dat.readline()
         instrucciones = instrucciones.partition(" ")
         instrucciones = instrucciones[2]
         print "La cantidad de instrucciones basicas fueron:" + instrucciones
-        out = open("out/Tp3(goloso).out","r")
+        out = open(archout,"r")
         resultado = out.readline()
-        resultado = resultado.partition(" ")
-        resultado = resultado[2]
-        print "La cantidad de nodos necesarios para realizar el recubrimiento fue:" + instrucciones
+        print "La cantidad de nodos necesarios para realizar el recubrimiento fue:" + resultado
     if algo == 3:
         listo = 0
         while listo == 0:
@@ -71,24 +75,45 @@ if(error == "false"):
             else:
                 listo = 1
         tiempoInicial = time.time()
-        proc = subprocess.Popen("java -jar busqueda_local.jar " + caso + " " + saco + " " + meto, shell=True)
+        proc = subprocess.Popen("java -jar busqueda_local.jar " + caso + " " +archout+ " " + archdat + " " + saco + " " + meto, shell=True)
         proc.wait()
         tiempoFinal = time.time()
         print "El algoritmo tardo(en ms):" + str(tiempoFinal - tiempoInicial)
-        dat = open("dat/Tp3(busqueda_local).dat","r")
+        dat = open(archdat,"r")
         instrucciones = dat.readline()
         instrucciones = instrucciones.partition(" ")
         instrucciones = instrucciones[2]
         print "La cantidad de instrucciones basicas fueron:" + instrucciones
-        out = open("out/Tp3(busqueda_local).out","r")
+        out = open(archout,"r")
         resultado = out.readline()
-        resultado = resultado.partition(" ")
-        resultado = resultado[2]
-        print "La cantidad de nodos necesarios para realizar el recubrimiento fue:" + instrucciones
+        print "La cantidad de nodos necesarios para realizar el recubrimiento fue:" + resultado
     if algo == 4:
+        listo = 0
+        while listo == 0:
+            it_max = raw_input("Cuantas iteraciones maximas quiere realizar?" )
+            it_sc = raw_input("Cuantas iteraciones sin cambio quiere permitir?")
+            meto = raw_input("Cuantos nodos quiere agregar?" )
+            saco = raw_input("Cuantos nodos quiere sacar?" )
+            if saco <= meto:
+                listo = 0
+                print "La cantidad que saca tiene que ser mayor a la que ingresa"
+            else:
+                listo = 1
+            porcentaje_goloso = raw_input("Cual quiere que sea la permisividad en el goloso?" )
         print "Corriendo GRASP"
-        tiempo = time()
-        #Aca va la parte del subprocess del algoritmo
+        tiempoInicial = time.time()
+        proc = subprocess.Popen("java -jar grasp.jar " + caso + " " +archout+ " " + archdat+ " " + it_max + " " + it_sc + " " + meto + " " + saco + " " + porcentaje_goloso, shell=True)
+        proc.wait()
+        tiempoFinal = time.time()
+        print "El algoritmo tardo(en ms):" + str(tiempoFinal - tiempoInicial)
+        dat = open(archdat,"r")
+        instrucciones = dat.readline()
+        instrucciones = instrucciones.partition(" ")
+        instrucciones = instrucciones[2]
+        print "La cantidad de instrucciones basicas fueron:" + instrucciones
+        out = open(archout,"r")
+        resultado = out.readline()
+        print "La cantidad de nodos necesarios para realizar el recubrimiento fue:" + resultado
 
 
 
